7d0c475c9ea7afbac1fae01433580637a40fa280
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2024-07-08 20:38:08 +0400

    feat: extend hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,18 @@ export default {
     extend: {
       colors:{
         inputColor:'#121829',
-        borderInput:'rgb(23 79 237)'
-      }
+        borderInput:'rgb(23 79 237)',
+        borderBox:'rgb(7, 232, 248)!important',
+        textColor:'rgb(7, 232, 248)',
+      },
+      boxShadow: {
+        'custom-hover': '0px 0px 19px rgb(0, 217, 255)',
+      },
+    },
+    variants: {
+      extend: {
+        boxShadow: ['hover'],
+      },
     },
   },
   plugins: [],
